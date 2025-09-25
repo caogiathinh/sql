bd0154a8925071c5deaeb3b1744ef1a2905f2e5e
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-09-25 08:58:05 +0700

    Aggregate function - Count

--- hands-on/Part2-SQL/Session03-AggregateFunction/01-Count.sql
+++ hands-on/Part2-SQL/Session03-AggregateFunction/01-Count.sql
@@ -67,3 +67,89 @@ SELECT COUNT(City) FROM Employees GROUP BY City --ĐÉM VALUE CỦA CITY, NHƯNG
 												--CHIA CITY THÀNH NHÓM RỒI ĐẾM TRONG NHÓM
 
 SELECT City ,COUNT(City) AS [No employees] FROM Employees GROUP BY City
+
+SELECT EmployeeID ,City ,COUNT(City) AS [No employees] FROM Employees GROUP BY City, EmployeeID
+-- vô nghĩa, chia nhóm theo key, mssv vô nghĩa. 
+--CHỐT HẠ: KHI XÀI HÀM GOM NHÓM, CÓ QUYỀN LIỆT KÊ TÊN CỘT LẺ Ở SELECT NHƯNG 
+--		   NHƯNG CỘT LẺ ĐÓ BẮT BUỘC PHẢI XUẤT HIỆN TRONG MỆNH ĐỀ GROUP BY
+--		   ĐỂ ĐẢM BẢO LOGIC: CỘT HIỂN THỊ | SỐ LƯỢNG ĐI KÈM, ĐẾM GOM THEO CỘT HIỂN THỊ MỚI LOGIC 
+--CỨ THEO CỘT CITY MÀ GOM, CỘT CITY NẰM Ở SELECT HỢP LÝ
+-- MUỐN HIỂN THỊ SỐ LƯỢNG CỦA AI ĐÓ, CÁI GÌ ĐÓ, GOM NHÓM THEO CÁI GÌ ĐÓ. 
+
+-- NẾU GOM THEO KEY, PK VÔ NGHĨA. KEY KHÔNG TRÙNG MỖI THẰNG 1 NHÓM ĐẾM CÁI GÌ ?
+-- MÃ SỐ SINH VIÊN: ---ĐẾM CÁI GÌ, VÔ NGHĨA. 
+-- MÃ CHUYÊN NGÀNH: -- ĐẾM SỐ SV CHUYÊN NGÀNH!!
+-- MÃ QUỐC GIA -- ĐẾM SỐ ĐƠN HÀNG 
+-- ĐIỂM THI -- ĐẾM SỐ LƯỢNG SV ĐẠT ĐIỂM ĐÓ
+-- CÓ CỘT ĐỂ GOM NHÓM, CỘT ĐÓ DÙNG ĐỂ HIỂN THỊ SỐ LƯỢNG KẾT QUẢ.
+
+
+-- IN RA MÃ NV 
+--1 London 1
+--2 Seatle 1
+--3 London 1
+--4        1
+--5        1
+SELECT City ,COUNT(City) AS [No employees] FROM Employees GROUP BY City
+
+-- 9. Hãy cho tôi biết thành phố nào có 2 nhân viên trở lên
+-- 2 chặng làm 
+-- 9.1. Các tp có bao nhiu nhân viên
+-- 9.2. Đếm xong mỗi thành phố, ta lọc lại kết quả sau đếm
+-- FILTER SAU ĐẾM, WHERE SAU ĐẾM, WHERE SAU KHI ĐÃ GOM NHÓM, AGGREGATE THÌ GỌI LÀ HAVING
+SELECT City ,COUNT(*) AS [No employees] FROM Employees GROUP BY City
+SELECT City ,COUNT(City) AS [No employees] FROM Employees GROUP BY City
+
+SELECT City ,COUNT(City) AS [No employees] FROM Employees GROUP BY City HAVING COUNT(*) >= 2
+
+--10. Đếm số nhân viên của 2 thành phố Seattle và London
+SELECT COUNT(City) AS [No employees] FROM Employees WHERE City IN('Seattle', 'London') GROUP BY City
+SELECT City ,COUNT(City) AS [No employees] FROM Employees WHERE City IN('Seattle', 'London') GROUP BY City
+
+--11. Trong 2 thành phố, thành phố nào nhiều hơn 3 nv
+SELECT City ,COUNT(City) AS [No employees] 
+FROM Employees 
+WHERE City IN('Seattle', 'London')
+GROUP BY City
+HAVING COUNT(*) >= 3
+
+-- 12. Đếm xem có bao nhiêu đơn hàng đã bán
+SELECT COUNT(*) AS [No Orders] FROM Orders
+SELECT COUNT(OrderID) AS [No Orders] FROM Orders
+-- 830 đơn hàng khác nhau, đếm mã đơn, hay đến cả cái đơn là như nhau. 
+-- cột có value NULL ăn hành
+
+--12.1. Nước Mĩ có bao nhiêu đơn hàng
+-- đi tìm Mĩ mà đếm, Lọc Mĩ rồi đếm
+-- KHÔNG PHẢI LÀ GOM CHIA NHÓM, KHÔNG CÓ MỖI QUỐC GIA BAO NHIÊU ĐƠN, 
+-- MỖI QUỐC GIA CÓ BAO NHIÊU ĐƠN, COUNT THEO QUỐC GIA, GROUP BY THEO QUỐC GIA
+SELECT COUNT(*) AS [No USA Orders] FROM Orders WHERE ShipCountry = 'USA'
+
+--12.2. Mĩ Anh Pháp chiếm tổng cộng bao nhiêu đơn hàng
+SELECT COUNT(*) [No USA, UK, France Orders] FROM Orders 
+WHERE ShipCountry IN('USA', 'France', 'UK')
+
+SELECT COUNT(*) FROM Orders WHERE ShipCountry = 'USA' 
+								OR ShipCountry = 'UK'
+								OR ShipCountry = 'France'
+
+--12.3 Mĩ Anh Pháp, mỗi quốc gia có bao nhiêu đơn hàng
+SELECT ShipCountry, COUNT(*) AS [No Orders] FROM Orders
+WHERE ShipCountry IN('USA', 'France', 'UK')
+GROUP BY ShipCountry
+
+--12.4 Trong 3 quốc gia A, P, M quốc gia nào có từ 100 đơn hàng trở lên
+SELECT ShipCountry, COUNT(*) AS [No Orders] FROM Orders
+WHERE ShipCountry IN('USA', 'France', 'UK')
+GROUP BY ShipCountry
+HAVING COUNT(*) >= 100
+
+--13. Đếm xem có bao nhiêu mặt hàng có trong kho
+--14. Đếm xem có bao nhiêu lượt quốc gia đã mua hàng 
+--15. Đếm xem có bao nhiêu quốc gia đã mua hàng (mỗi quốc gia đếm một lần)
+--16. Đếm số lượng đơn hàng của mỗi quốc gia
+--17. Quốc gia nào có từ 10 đơn hàng trở lên
+--18. Đếm xem mỗi chủng loại hàng có bao nhiêu mặt hàng (bia rượu có 5 sp, thủy sản 10 sp)
+--19. Trong 3 quốc gia A P M, quốc gia nào có nhiều đơn hàng nhất
+--20. Quốc gia nào có nhiều đơn hàng nhất
+--21. Thành phố nào có nhiều nhân viên nhất???
