56a0c5816e6f131403331eeac9f3f67c8993160e
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-09-25 20:20:37 +0700

    Update Aggregate function - Sum-Min-Max-AVG

--- hands-on/Part2-SQL/Session03-AggregateFunction/02-MinMaxSumAvg.sql
+++ hands-on/Part2-SQL/Session03-AggregateFunction/02-MinMaxSumAvg.sql
@@ -77,5 +77,65 @@ WHERE Freight >= (
 				  )
 --chỉ những thằng lớn hơn trung bình thì mới đếm
 
+-- CỘT XUẤT HIỆN TRONG SELECT HÀM Ý ĐẾM THEO CỘT NÀY 
+-- TỈNH <ĐẾM CÁI GÌ ĐÓ CỦA TỈNH> -> CHIA THEO TỈNH MÀ ĐẾM
+--									GROUP BY TỈNH
+-- CHUYÊN NGÀNH, <ĐẾM CỦA CHUYÊN NGÀNH> -> CHIA THEO CHUYÊN NGÀNH MÀ ĐẾM 
+--										  GROUP BY CHUYÊN NGÀNH
+-- CÓ QUYỀN GROUP BY TRÊN NHIỀU CỘT 
 
+-- MÃ CHUYÊN NGÀNH, TÊN CHUYÊN NGÀNH <SL,SV> -> GROUP BY MÃ CN, TÊN CN
+
+-- ÔN TẬP THÊM
+-- 1. In ra thông tin nhân viên
+SELECT * FROM Employees
+
+--2. Đếm xem mỗi khu vực có bao nhiêu nhân viên 
+SELECT COUNT(*) FROM Employees ORDER BY Region
+SELECT COUNT(*) FROM Employees GROUP BY Region -- 4 (null) 5 (WA)
+										-- 2 NHÓM REGION, 2 CỤM REGION: WA, NULL
+SELECT COUNT(Region) FROM Employees GROUP BY Region -- 2 cụm NULL VÀ WA
+-- 0 5, do NULL KHÔNG ĐƯỢC XEM LÀ VALUE ĐỂ ĐẾM , NHƯNG VẪN LÀ 1 VALUE ĐỂ DC CHIA NHÓM
+--												 NHÓM KHÔNG GIÁ TRỊ
+
+SELECT Region, COUNT(Region) FROM Employees GROUP BY Region --sai do đếm null
+SELECT Region, COUNT(*) FROM Employees GROUP BY Region --đúng do đếm dòng
+
+--3. Khảo sát đơn hàng
+SELECT * FROM Orders
+-- Mỗi quốc gia có bao nhiu đơn hàng
+SELECT ShipCountry, COUNT(*) AS [Total Orders] FROM Orders GROUP BY ShipCountry
+
+--4. Quốc gia nào có từ 100 đơn hàng ? 
+SELECT ShipCountry, COUNT(*) AS [Total Orders] FROM Orders GROUP BY ShipCountry 
+HAVING COUNT(*) >= 100
+
+--5. Quốc gia nào có nhiều đơn hàng nhất ? 
+SELECT MAX([Total Orders])
+FROM 
+(
+	SELECT ShipCountry, COUNT(*) AS [Total Orders] FROM Orders GROUP BY ShipCountry 
+) AS [Cntry]
+-- lấy được max rồi 
+SELECT ShipCountry, COUNT(*) AS [Total Orders] FROM Orders GROUP BY ShipCountry 
+HAVING COUNT(*) >= (
+						SELECT MAX([Total Orders])
+						FROM 
+						(
+							SELECT ShipCountry, COUNT(*) AS [Total Orders] FROM Orders GROUP BY ShipCountry 
+						) AS [Cntry]
+					)
+
+--6. Liệt kê các đơn hàng của kh mã số VINET
+SELECT * FROM Orders WHERE CustomerID = 'VINET'
+
+--7. VINET đã mua bao nhiêu lần ?? 
+SELECT CustomerID, COUNT(*) AS [Totals orders] FROM Orders
+											   GROUP BY CustomerID
+											   HAVING CustomerID = 'VINET'
+
+SELECT CustomerID, COUNT(*) AS [Totals orders] FROM Orders 
+											   WHERE CustomerID = 'VINET'
+											   GROUP BY CustomerID
+					
 
